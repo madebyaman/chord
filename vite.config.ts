98dc2dcc3024e15e8c370a825e1788d96d5396c1
Aman Thakur <builtbyaman@gmail.com>
2025-10-19 12:09:24 +0530

    disable react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,12 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
+    react(),
+    // react({
+    //   babel: {
+    //     plugins: [["babel-plugin-react-compiler"]],
+    //   },
+    // }),
   ],
   test: {
     browser: {
